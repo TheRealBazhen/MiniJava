08ffdf8d65e6e8060440edd9c1d474b79b2764ea
TheRealBazhen <andrej.bazhenov.ab1@gmail.com>
2020-08-02 22:14:12 +0300

    Simplify getting type name from AST

--- program/types/types.cpp
+++ program/types/types.cpp
@@ -7,9 +7,17 @@ void SimpleType::Accept(std::shared_ptr<Visitor> visitor) {
     visitor->Visit(shared_from_this());
 }
 
+std::string SimpleType::GetTypeName() {
+    return name;
+}
+
 ArrayType::ArrayType(std::shared_ptr<SimpleType> element_type) : element_type(element_type) {
 }
 
 void ArrayType::Accept(std::shared_ptr<Visitor> visitor) {
     visitor->Visit(shared_from_this());
+}
+
+std::string ArrayType::GetTypeName() {
+    return element_type->name + "[]";
 }
--- program/types/types.h
+++ program/types/types.h
@@ -4,6 +4,8 @@
 #include <program/node.h>
 
 class GrammaticsType : public Node {
+public:
+    virtual std::string GetTypeName() = 0;
 };
 
 class SimpleType : public GrammaticsType, public std::enable_shared_from_this<SimpleType> {
@@ -11,6 +13,8 @@ public:
     SimpleType(const std::string& name);
 
     void Accept(std::shared_ptr<Visitor> visitor) override;
+
+    std::string GetTypeName() override;
 
     std::string name;
 };
@@ -20,6 +24,8 @@ public:
     ArrayType(std::shared_ptr<SimpleType> element_type);
 
     void Accept(std::shared_ptr<Visitor> visitor) override;
+
+    std::string GetTypeName() override;
 
     std::shared_ptr<SimpleType> element_type;
 };
